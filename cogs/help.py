2f0e675bc041f080fcda503cbc314670509a357d
Karsten Courtney <ftwkrc@gmail.com>
2020-06-16 13:05:11 -0400

    Edited the explanation for the commands in help

--- cogs/help.py
+++ cogs/help.py
@@ -13,9 +13,9 @@ class help(commands.Cog):
 
         embed.set_footer(text=f"{ctx.author.display_name}", icon_url=ctx.author.avatar_url)
         embed.set_author(name="Miska Bot - Help and Documentation", icon_url=self.bot.user.avatar_url)
-        embed.add_field(name="Command: upload", value="Uploads your animal pic to the bot to use", inline=False)
-        embed.add_field(name="Command: pic", value="Return a random picyure from animal pictures that have been uploaded optionally you can specify a pictures id", inline=False)
-        embed.add_field(name="Command: delete", value="Deletes a uploaded animal picture specified by the pictures id", inline=False)
+        embed.add_field(name="Command: upload", value="Uploads your animal picture", inline=False)
+        embed.add_field(name="Command: pic", value="Return a random picture from animal pictures that have been uploaded optionally you can specify a pictures id", inline=False)
+        embed.add_field(name="Command: delete", value="Deletes a uploaded animal picture", inline=False)
         embed.add_field(name="Command: prefix", value="Specifies a new prefix for the server", inline=False)
 
         await ctx.send(embed=embed)
