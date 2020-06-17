e266d2051565a474edea835e7eef47b99b242db8
Karsten Courtney <ftwkrc@gmail.com>
2020-06-17 14:12:39 -0400

    Added in error handling to make sure the user has attached a image along with makig sure they upload one photo before using pic or delete

--- cogs/animalPics.py
+++ cogs/animalPics.py
@@ -13,6 +13,14 @@ class pictureIdError(commands.CommandError):
     pass
 
 
+class emptyPicArr(commands.CommandError):
+    pass
+
+
+class noPictures(commands.CommandError):
+    pass
+
+
 class animalPics(commands.Cog):
     def __init__(self, bot):
         self.bot = bot
@@ -20,29 +28,38 @@ class animalPics(commands.Cog):
     @commands.command()
     async def upload(self, ctx):
         await ctx.channel.trigger_typing()
-        with open(jsonFile, 'r') as f:
-            miskaJSON = json.load(f)
+        try:
+            with open(jsonFile, 'r') as f:
+                miskaJSON = json.load(f)
 
-        archiveOfPics = self.bot.get_user(327205633319239681)
+            archiveOfPics = self.bot.get_user(327205633319239681)
 
-        idArr = []
+            idArr = []
 
-        for x in ctx.message.attachments:
-            animalPicURL = x.url
+            if len(ctx.message.attachments) == 0:
+                raise noPictures
 
-            miskaJSON[str(ctx.guild.id)]["animalURLS"].append([ctx.author.name, animalPicURL])
-            idArr.append(str(len(miskaJSON[str(ctx.guild.id)]["animalURLS"])))
-            async with aiohttp.ClientSession() as session:                                     #start of archival to make sure the link is valid
-                async with session.get(animalPicURL) as r:                                     #
-                    if r.status != 200:                                                        #
-                        return await ctx.send('Failed to archive...')                          #
-                    photo = io.BytesIO(await r.read())                                         #
-                    await archiveOfPics.send(file=discord.File(photo, 'archive.jpg'))          #End of archival to make sure the is link valid
+            for x in ctx.message.attachments:
+                animalPicURL = x.url
 
-        with open(jsonFile, 'w') as f:
-            json.dump(miskaJSON, f, indent=4)
-        ids = '/'.join(idArr)
-        await ctx.send(f'Successfully uploaded :thumbsup: ID: {ids}')
+                miskaJSON[str(ctx.guild.id)]["animalURLS"].append([ctx.author.name, animalPicURL])
+                idArr.append(str(len(miskaJSON[str(ctx.guild.id)]["animalURLS"])))
+                async with aiohttp.ClientSession() as session:                                     #start of archival to make sure the link is valid
+                    async with session.get(animalPicURL) as r:                                     #
+                        if r.status != 200:                                                        #
+                            return await archiveOfPics.send('Failed to archive...')                #
+                        photo = io.BytesIO(await r.read())                                         #
+                        await archiveOfPics.send(file=discord.File(photo, 'archive.jpg'))          #End of archival to make sure the is link valid
+
+            with open(jsonFile, 'w') as f:
+                json.dump(miskaJSON, f, indent=4)
+            ids = '/'.join(idArr)
+
+            await ctx.send(f'Successfully uploaded :thumbsup: ID: {ids}')
+        except noPictures:
+            await ctx.message.delete()
+            await ctx.send(f'{ctx.author.mention}, please attach a image to be uploaded',
+                           delete_after=15)
 
     @commands.command()
     @commands.cooldown(5, 1, commands.BucketType.user)
@@ -52,24 +69,34 @@ class animalPics(commands.Cog):
             miskaJSON = json.load(f)
 
         if id == -1 or id == None:
-            picId = random.randint(0, len(miskaJSON[str(ctx.guild.id)]["animalURLS"]))
-            picArr = miskaJSON[str(ctx.guild.id)]["animalURLS"][picId]
-            picAuthor = picArr[0]
-            picURL = picArr[1]
+            try:
+                if len(miskaJSON[str(ctx.guild.id)]["animalURLS"]) == 0:
+                    raise emptyPicArr
 
-            embed = discord.Embed(
-                title=f"Animal Picture",
-                description=f"Picture uploaded by {picAuthor} | ID: {picId+1}",
-                color=discord.Color.blue()
-            )
-            embed.set_image(url=picURL)
-            embed.set_footer(text=ctx.author.display_name, icon_url=ctx.author.avatar_url)
+                picId = random.randint(0, len(miskaJSON[str(ctx.guild.id)]["animalURLS"]))
+                picArr = miskaJSON[str(ctx.guild.id)]["animalURLS"][picId]
+                picAuthor = picArr[0]
+                picURL = picArr[1]
 
-            await ctx.send(embed=embed)
+                embed = discord.Embed(
+                    title=f"Animal Picture",
+                    description=f"Picture uploaded by {picAuthor} | ID: {picId+1}",
+                    color=discord.Color.blue()
+                )
+                embed.set_image(url=picURL)
+                embed.set_footer(text=ctx.author.display_name, icon_url=ctx.author.avatar_url)
+
+                await ctx.send(embed=embed)
+            except emptyPicArr:
+                await ctx.message.delete()
+                await ctx.send(f'{ctx.author.mention}, please upload a picture using the *upload* command',
+                               delete_after=15)
         else:
             try:
                 if id > len(miskaJSON[str(ctx.guild.id)]["animalURLS"]) or id <= 0:
                     raise pictureIdError
+                if len(miskaJSON[str(ctx.guild.id)]["animalURLS"]) == 0:
+                    raise emptyPicArr
 
                 picArr = miskaJSON[str(ctx.guild.id)]["animalURLS"][id-1]
                 picAuthor = picArr[0]
@@ -88,6 +115,10 @@ class animalPics(commands.Cog):
                 await ctx.message.delete()
                 await ctx.send(f'{ctx.author.mention}, you gave a picture id that does not exist',
                                delete_after=15)
+            except emptyPicArr:
+                await ctx.message.delete()
+                await ctx.send(f'{ctx.author.mention}, please upload a picture using the *upload* command',
+                               delete_after=15)
 
     @commands.command()
     @commands.has_permissions(manage_messages=True)
@@ -100,6 +131,8 @@ class animalPics(commands.Cog):
 
             if id > len(miskaJSON[str(ctx.guild.id)]["animalURLS"]) or id <= 0:
                 raise pictureIdError
+            if len(miskaJSON[str(ctx.guild.id)]["animalURLS"]) == 0:
+                raise emptyPicArr
 
             miskaJSON[str(ctx.guild.id)]["animalURLS"].pop(id-1)
 
@@ -111,7 +144,10 @@ class animalPics(commands.Cog):
             await ctx.message.delete()
             await ctx.send(f'{ctx.author.mention}, you gave a picture id that does not exist',
                            delete_after=15)
-
+        except emptyPicArr:
+            await ctx.message.delete()
+            await ctx.send(f'{ctx.author.mention}, please upload a picture using the *upload* command',
+                           delete_after=15)
 
     @delete.error
     async def delete_error(self, ctx, error):
